e6e4e200d9026ce188d26d6dcaa39da1ef738eae
Lassi Jokinen <lassijokinen00@gmail.com>
2026-06-11 18:46:08 +0300

    Fixed bad variable

--- Perks System - Call of Duty Perks/lua/autorun/codperkacolanetworking.lua
+++ Perks System - Call of Duty Perks/lua/autorun/codperkacolanetworking.lua
@@ -1,51 +1,51 @@
 net.Receive("PerkACola", function(len, ply)
-Player = net.ReadEntity()
+PlyEnt = net.ReadEntity()
 Perk = net.ReadString()
 
-if Player:GetNWString("Perk1", "None") != "Juggernog" and Perk == "Juggernog" then
-	Player:SetNWString("Perk1", "Juggernog")
-elseif Player:GetNWString("Perk1", "None") == "Juggernog" and Perk == "Juggernog" then
-	Player:SetNWString("Perk1", "None")
+if PlyEnt:GetNWString("Perk1", "None") != "Juggernog" and Perk == "Juggernog" then
+	PlyEnt:SetNWString("Perk1", "Juggernog")
+elseif PlyEnt:GetNWString("Perk1", "None") == "Juggernog" and Perk == "Juggernog" then
+	PlyEnt:SetNWString("Perk1", "None")
 end
-if Player:GetNWString("Perk2", "None") != "Double Tap" and Perk == "Double Tap" then
-	Player:SetNWString("Perk2", "Double Tap")
-elseif Player:GetNWString("Perk2", "None") == "Double Tap" and Perk == "Double Tap" then
-	Player:SetNWString("Perk2", "None")
+if PlyEnt:GetNWString("Perk2", "None") != "Double Tap" and Perk == "Double Tap" then
+	PlyEnt:SetNWString("Perk2", "Double Tap")
+elseif PlyEnt:GetNWString("Perk2", "None") == "Double Tap" and Perk == "Double Tap" then
+	PlyEnt:SetNWString("Perk2", "None")
 end
-if Player:GetNWString("Perk3", "None") != "Stamin-Up" and Perk == "Stamin-Up" then
-	Player:SetNWString("Perk3", "Stamin-Up")
-elseif Player:GetNWString("Perk3", "None") == "Stamin-Up" and Perk == "Stamin-Up" then
-	Player:SetNWString("Perk3", "None")
+if PlyEnt:GetNWString("Perk3", "None") != "Stamin-Up" and Perk == "Stamin-Up" then
+	PlyEnt:SetNWString("Perk3", "Stamin-Up")
+elseif PlyEnt:GetNWString("Perk3", "None") == "Stamin-Up" and Perk == "Stamin-Up" then
+	PlyEnt:SetNWString("Perk3", "None")
 end
-if Player:GetNWString("Perk4", "None") != "Deadshot Daquiri" and Perk == "Deadshot Daquiri" then
-	Player:SetNWString("Perk4", "Deadshot Daquiri")
-elseif Player:GetNWString("Perk4", "None") == "Deadshot Daquiri" and Perk == "Deadshot Daquiri" then
-	Player:SetNWString("Perk4", "None")
+if PlyEnt:GetNWString("Perk4", "None") != "Deadshot Daquiri" and Perk == "Deadshot Daquiri" then
+	PlyEnt:SetNWString("Perk4", "Deadshot Daquiri")
+elseif PlyEnt:GetNWString("Perk4", "None") == "Deadshot Daquiri" and Perk == "Deadshot Daquiri" then
+	PlyEnt:SetNWString("Perk4", "None")
 end
-if Player:GetNWString("Perk5", "None") != "PhD Flopper" and Perk == "PhD Flopper" then
-	Player:SetNWString("Perk5", "PhD Flopper")
-elseif Player:GetNWString("Perk5", "None") == "PhD Flopper" and Perk == "PhD Flopper" then
-	Player:SetNWString("Perk5", "None")
+if PlyEnt:GetNWString("Perk5", "None") != "PhD Flopper" and Perk == "PhD Flopper" then
+	PlyEnt:SetNWString("Perk5", "PhD Flopper")
+elseif PlyEnt:GetNWString("Perk5", "None") == "PhD Flopper" and Perk == "PhD Flopper" then
+	PlyEnt:SetNWString("Perk5", "None")
 end
-if Player:GetNWString("Perk6", "None") != "Electric Cherry" and Perk == "Electric Cherry" then
-	Player:SetNWString("Perk6", "Electric Cherry")
-elseif Player:GetNWString("Perk6", "None") == "Electric Cherry" and Perk == "Electric Cherry" then
-	Player:SetNWString("Perk6", "None")
+if PlyEnt:GetNWString("Perk6", "None") != "Electric Cherry" and Perk == "Electric Cherry" then
+	PlyEnt:SetNWString("Perk6", "Electric Cherry")
+elseif PlyEnt:GetNWString("Perk6", "None") == "Electric Cherry" and Perk == "Electric Cherry" then
+	PlyEnt:SetNWString("Perk6", "None")
 end
-if Player:GetNWString("Perk7", "None") != "Vulture Aid" and Perk == "Vulture Aid" then
-	Player:SetNWString("Perk7", "Vulture Aid")
-elseif Player:GetNWString("Perk7", "None") == "Vulture Aid" and Perk == "Vulture Aid" then
-	Player:SetNWString("Perk7", "None")
+if PlyEnt:GetNWString("Perk7", "None") != "Vulture Aid" and Perk == "Vulture Aid" then
+	PlyEnt:SetNWString("Perk7", "Vulture Aid")
+elseif PlyEnt:GetNWString("Perk7", "None") == "Vulture Aid" and Perk == "Vulture Aid" then
+	PlyEnt:SetNWString("Perk7", "None")
 end
-if Player:GetNWString("Perk8", "None") != "Death Perception" and Perk == "Death Perception" then
-	Player:SetNWString("Perk8", "Death Perception")
-elseif Player:GetNWString("Perk8", "None") == "Death Perception" and Perk == "Death Perception" then
-	Player:SetNWString("Perk8", "None")
+if PlyEnt:GetNWString("Perk8", "None") != "Death Perception" and Perk == "Death Perception" then
+	PlyEnt:SetNWString("Perk8", "Death Perception")
+elseif PlyEnt:GetNWString("Perk8", "None") == "Death Perception" and Perk == "Death Perception" then
+	PlyEnt:SetNWString("Perk8", "None")
 end
-if Player:GetNWString("Perk9", "None") != "Elemental Pop" and Perk == "Elemental Pop" then
-	Player:SetNWString("Perk9", "Elemental Pop")
-elseif Player:GetNWString("Perk9", "None") == "Elemental Pop" and Perk == "Elemental Pop" then
-	Player:SetNWString("Perk9", "None")
+if PlyEnt:GetNWString("Perk9", "None") != "Elemental Pop" and Perk == "Elemental Pop" then
+	PlyEnt:SetNWString("Perk9", "Elemental Pop")
+elseif PlyEnt:GetNWString("Perk9", "None") == "Elemental Pop" and Perk == "Elemental Pop" then
+	PlyEnt:SetNWString("Perk9", "None")
 end
 
 end)
